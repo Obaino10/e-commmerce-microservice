034433354a3ccf9012e44abd2f034cf6aef656b8
Obaino10 <obaino4real1022@gmail.com>
2023-02-13 21:49:58 +0100

    Implementation of an Event Driven Architecture using Kafka

--- order-service/src/main/java/com/obinna/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/obinna/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.obinna.orderservice.service;
 import com.obinna.orderservice.dto.InventoryResponse;
 import com.obinna.orderservice.dto.OrderLinesItemsDto;
 import com.obinna.orderservice.dto.OrderRequest;
+import com.obinna.orderservice.event.OrderPlacedEvent;
 import com.obinna.orderservice.model.Order;
 import com.obinna.orderservice.model.OrderLineItems;
 import com.obinna.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -25,6 +27,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -57,6 +60,7 @@ public class OrderService {
 
             if (allProductsInStock) {
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order Placed Successfully";
             }else {
                 throw new IllegalArgumentException("Product is not in stock, please check back later");
